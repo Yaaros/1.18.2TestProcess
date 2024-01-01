f2290e55850698cdb6af130b01d1d7439e8efb73
Yaaros <115866801+Yaaros@users.noreply.github.com>
2024-01-01 21:07:31 +0800

    .md change

--- src/main/java/com/yaaros/ctm_undefined/entity/ModEntity.java
+++ src/main/java/com/yaaros/ctm_undefined/entity/ModEntity.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+//未使用
 public class ModEntity {
     public static final DeferredRegister<EntityType<?>> ENTITIES =
             DeferredRegister.create(ForgeRegistries.ENTITIES, Main.MOD_ID);
